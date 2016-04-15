0d0d066907ae45a0b301dd791401182a92cb7ae2
Michael Coombes <mc6415@gmail.com>
2016-04-15 14:50:00 +0100

    Saves output to html file
    Now also saves the answers to a HTML file

--- ProjectEuler/Module1.vb
+++ ProjectEuler/Module1.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Reflection
+Imports System.Text
+Imports System.IO
 
 Module Module1
     Sub Main()
@@ -16,6 +18,9 @@ End Module
 Public Class DoWork
     Public Function PerformCalcs()
         Dim method As System.Reflection.MethodInfo
+        Dim sb = New StringBuilder()
+        sb.Append("<table>")
+        sb.Append("<thead><th>Problem</th><th>Solution</th></thead><tbody>")
         Dim funcs = Assembly.GetExecutingAssembly.GetTypes().Where(Function(t) t.Name.Contains("Problem")).ToList()
         Dim i = 1
         For Each func In funcs
@@ -24,9 +29,25 @@ Public Class DoWork
                 Dim watch = Stopwatch.StartNew()
                 Dim result = method.Invoke(Me, Nothing)
                 watch.Stop()
+                sb.Append("<tr>")
+                sb.Append("<td>" + i.ToString + "</td><td>" + result.ToString + "</td></tr>")
                 printTime(watch.ElapsedMilliseconds, i, result)
                 i += 1
             End If
         Next
+        sb.Append("</tbody></table>")
+        Dim file As New FileInfo("./out.html")
+        If Not file.Exists Then
+            Dim f = file.Open(FileMode.CreateNew)
+            f.Close()
+            Dim write = file.CreateText()
+            write.Write(sb.ToString)
+            write.Close()
+        Else
+            Dim test = file.CreateText()
+            test.Write(sb.ToString)
+            test.Close()
+        End If
+        Console.WriteLine("Answers saved in: " + file.FullName)
     End Function
 End Class
