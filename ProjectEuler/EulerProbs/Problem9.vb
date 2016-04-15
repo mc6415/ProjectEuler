16b4d42e86943f8d5e4ad1f8e93ebd00d9a2c97b
Michael Coombes <mc6415@gmail.com>
2016-04-15 12:41:56 +0100

    Added solution for problem 9 :newspaper:

---
+++ ProjectEuler/EulerProbs/Problem9.vb
@@ -0,0 +1,16 @@
+﻿'There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+'Find the product abc.
+Public Class Problem9
+    Public Shared Function Calc()
+        ' If a is the smallest digit, it must always be under 500
+        For a = 1 To 1000 / 2
+            ' b is always larger than a
+            For b = a + 1 To 1000 / 2
+                Dim c = 1000 - a - b
+                If (a * a) + (b * b) = (c * c) Then
+                    Return a * b * c
+                End If
+            Next
+        Next
+    End Function
+End Class
