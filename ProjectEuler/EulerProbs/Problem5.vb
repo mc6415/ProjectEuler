d3498235bee0fef4d859db0f1359c1e201f4c202
Michael Coombes <mc6415@gmail.com>
2016-04-15 11:10:08 +0100

    Added solution for problem 5

--- ProjectEuler/EulerProbs/Problem5.vb
+++ ProjectEuler/EulerProbs/Problem5.vb
@@ -1,8 +1,10 @@
 ﻿Public Class Problem5
     Public Shared Function Calc()
-        Enumerable.Range(1, 19).ToList()
-
-        For Each num In Enumerable.Range(1, 20).ToList()
+        ' Most concise way I could think of writing it, probably a faster way somewhere
+        For i = 2520 To Integer.MaxValue Step 20
+            If Enumerable.Range(1, 20).Where(Function(j) i Mod j <> 0).ToList().Count = 0 Then
+                Return i
+            End If
         Next
     End Function
 End Class
