d97ac1850109e415fd4bcdce35005c2232f031b3
Michael Coombes <mc6415@gmail.com>
2016-04-15 11:39:00 +0100

    Added solution to Problem 7 :pineapple:

---
+++ ProjectEuler/EulerProbs/Problem7.vb
@@ -0,0 +1,15 @@
+﻿'What is the 10 001st prime number?
+Public Class Problem7
+    Public Shared Function Calc()
+        ' Offset i by 1 as the below will miss out 2 which is Prime
+        Dim i = 1
+        For num = 1 To Integer.MaxValue Step 2
+            If EulerMaths.checkPrime(num) Then
+                i += 1
+                If i = 10001 Then
+                    Return num
+                End If
+            End If
+        Next
+    End Function
+End Class
